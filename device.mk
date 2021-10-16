fa44ad6a900ab1d99cc0c21527b0a69648af0c4f
Ctapchuk <pilipenko0409@gmail.com>
2021-10-17 00:36:46 +0700

    Return compilation of some prebuiled libs

--- device.mk
+++ device.mk
@@ -38,12 +38,16 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hidl.base@1.0 \
     libandroidicu \
+    libcap \
     libion \
+    libpcrecpp \
     libxml2
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.base@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpcrecpp.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
 
 #
